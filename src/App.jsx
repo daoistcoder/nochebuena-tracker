91c71afd0ad772029648f687e1c79fcfccd3f68b
daoistcoder <deservekindness@gmail.com>
2023-12-18 07:37:23 +0800

    FIX: error in await

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,34 @@
 import './App.css'
 
 import { FoodCreateForm } from './ui-components'
+import { useEffect, useState } from 'react';
+import { DataStore } from '@aws-amplify/datastore';
+import { Food } from './models';
 
 function App() {
+  const [food, setFood] = useState([]);
+
+  useEffect(() => {
+    const getData = () => {
+      DataStore.observeQuery(Food).subscribe(({ items }) => {
+        setFood(items)
+      })
+    }
+    getData()
+  })
+
   return (
-    <FoodCreateForm style={styles.container} />
+    <div style={styles.container}>
+      <FoodCreateForm />
+      <div>
+        <h2>Foods</h2>
+        <ul>
+          {food.map(item => {
+            return <li key={item.id}>{item.title} - {item.person}</li>
+          })}
+        </ul>
+      </div>
+    </div>
   )
 }
 
@@ -14,6 +38,7 @@ const styles = {
     border: '1px solid #ccc', // Add a border
     boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)', // Add a box-shadow
     borderRadius: '5px',
+    marginBottom: '20vh', // Add some margin at the bottom
   },
 };
 
