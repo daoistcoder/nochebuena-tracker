0d97461b363a0e11044a1f974bc23e612f7d3be9
daoistcoder <deservekindness@gmail.com>
2023-12-18 09:30:24 +0800

    UPDATE: UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
 
 import { FoodCreateForm } from './ui-components'
-import { Heading } from '@aws-amplify/ui-react'
+import { Alert, Heading } from '@aws-amplify/ui-react'
 import { useEffect, useState } from 'react';
 import { DataStore } from '@aws-amplify/datastore';
 import { Food, Category } from './models';
 
 function App() {
   const [food, setFood] = useState([]);
+  const [showForm, setShowForm] = useState(true);
 
   useEffect(() => {
     const getData = async () => {
@@ -21,12 +22,18 @@ function App() {
   return (
     <div style={styles.container}>
       <Heading level={1} style={styles.head1}>Noche Buena 🎄</Heading>
-      <div style={styles.formContainer}>
-        <FoodCreateForm />
-      </div>
+      {showForm ? (
+        <div style={styles.formContainer}>
+          <FoodCreateForm onSuccess={() => {
+            setShowForm(false);
+          }}/>
+        </div>
+      ) : (
+        <Alert variation='success' style={styles.alerts}>Salamat sa pagvolunteer! kita kits sa Pasko!</Alert>
+      )}
       <div style={styles.formContainer2}>
         <Heading level={3}>Mga Handa 🍽️</Heading>
-        <div style={styles.category}>
+        <div style={styles.categories}>
           {Object.entries(Category).map(([category]) => {
             // Filter the food array to get items in this category
             const itemsInCategory = food.filter(item => item.category === category);
@@ -38,8 +45,8 @@ function App() {
 
             // If there are items in this category, return the category and its items
             return (
-              <div key={category}>
-                <Heading level={5}>{category}</Heading>
+              <div key={category} style={styles.category}>
+                <Heading level={5}>{category.charAt(0).toUpperCase() + category.slice(1).toLocaleLowerCase()}</Heading>
                 <ul>
                   {food.map(item => {
                     if (item.category === category) {
@@ -72,7 +79,7 @@ const styles = {
   },
 
   formContainer2: { // css for sub container with 
-    width: '100%',
+    width: '44vw',
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'start',
@@ -80,15 +87,25 @@ const styles = {
     height: '30vh',
   },
   // css for category columns
-  category: {
+  categories: {
     width: '100%',
     display: 'grid',
     gridTemplateColumns: '1fr 1fr 1fr',
     gap: '1rem',
     padding: '0 1rem',
   },
+  // css for category columns
+  category: {
+    borderRadius: '5px',
+    backgroundColor: 'hsl(130, 60%, 95%)',
+  },
   head1: {
     textAlign: 'center',
+    marginBottom: '1vh',
+  },
+  alerts: {
+    fontSize: '1.5rem',
+    width: '750px',
     marginBottom: '1vh',
   },
 };
