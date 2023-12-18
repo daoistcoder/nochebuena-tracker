021b301dcb3f3f76230dc4c8a52502219fbb62ff
daoistcoder <deservekindness@gmail.com>
2023-12-18 08:18:26 +0800

    UPDATE: ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import './App.css'
 
 import { FoodCreateForm } from './ui-components'
+import { Heading } from '@aws-amplify/ui-react'
 import { useEffect, useState } from 'react';
 import { DataStore } from '@aws-amplify/datastore';
-import { Food } from './models';
+import { Food, Category } from './models';
 
 function App() {
   const [food, setFood] = useState([]);
@@ -19,14 +20,37 @@ function App() {
 
   return (
     <div style={styles.container}>
-      <FoodCreateForm />
-      <div>
-        <h2>Foods</h2>
-        <ul>
-          {food.map(item => {
-            return <li key={item.id}>{item.title} - {item.person}</li>
+      <Heading level={1} style={styles.head1}>Noche Buena 🎄</Heading>
+      <div style={styles.formContainer}>
+        <FoodCreateForm />
+      </div>
+      <div style={styles.formContainer2}>
+        <Heading level={3}>Mga Handa 🍽️</Heading>
+        <div style={styles.category}>
+          {Object.entries(Category).map(([category]) => {
+            // Filter the food array to get items in this category
+            const itemsInCategory = food.filter(item => item.category === category);
+
+            // If there are no items in this category, return null
+            if (itemsInCategory.length === 0) {
+              return null;
+            }
+
+            // If there are items in this category, return the category and its items
+            return (
+              <div key={category}>
+                <Heading level={5}>{category}</Heading>
+                <ul>
+                  {food.map(item => {
+                    if (item.category === category) {
+                      return <li key={item.id}>{item.title} - {item.person}</li>
+                    }
+                  })}
+                </ul>
+              </div>
+            );
           })}
-        </ul>
+        </div>
       </div>
     </div>
   )
@@ -34,11 +58,37 @@ function App() {
 
 const styles = {
   container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    gap: '1vh',
+    height: '100vh',
+  },
+  formContainer: {
     height: '45vh',
     border: '1px solid #ccc', // Add a border
     boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)', // Add a box-shadow
     borderRadius: '5px',
-    marginBottom: '20vh', // Add some margin at the bottom
+  },
+
+  formContainer2: { // css for sub container with 
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'start',
+    gap: '1vh',
+    height: '30vh',
+  },
+  // css for category columns
+  category: {
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr 1fr',
+    gap: '2vw',
+    padding: '1rem',
+  },
+  head1: {
+    textAlign: 'center',
+    marginBottom: '1vh',
   },
 };
 
